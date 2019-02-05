68fa4654f029df613a0353c3c402799b40bb05dd
ranu <ranufirman98@gmail.com>
2019-02-05 21:55:32 +0700

    database

--- asset/db/tanaman(1).sql
+++ asset/db/tanaman(1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2018 at 06:25 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Feb 05, 2019 at 03:53 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,43 @@ SET time_zone = "+00:00";
 --
 -- Database: `tanaman`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bukti`
+--
+
+CREATE TABLE `bukti` (
+  `id_bukti` int(25) DEFAULT NULL,
+  `id_faktur` int(25) DEFAULT NULL,
+  `img` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faktur`
+--
+
+CREATE TABLE `faktur` (
+  `id_faktur` char(15) NOT NULL,
+  `id_pelanggan` char(15) NOT NULL,
+  `id_pesanan` char(15) NOT NULL,
+  `status` int(2) NOT NULL,
+  `total` int(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faktur`
+--
+
+INSERT INTO `faktur` (`id_faktur`, `id_pelanggan`, `id_pesanan`, `status`, `total`) VALUES
+('15424437301508', '215424435224', '1542443730', 2, 105000),
+('15424442184338', '215424441974', '1542444218', 1, 220000),
+('15424558671971', '215424558474', '1542455867', 0, 105000),
+('15425079275447', '215425078974', '1542507927', 0, 105000),
+('15425191575666', '215425191424', '1542519157', 0, 105000);
 
 -- --------------------------------------------------------
 
@@ -42,8 +79,11 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `jenis_kelamin`, `alamat`, `email`, `no_hp`) VALUES
-('215423853744', 'Bunga permatasari', 'Perempuan', 'Jalan Jambu 7 no 10 Perumnas 2 RT. 004/008 Parungpanjang Bogor 16360 Jawa Barat', 'adibrata.developer@gmail.com', '0219288300404'),
-('215423857234', 'Andriani', 'Perempuan', 'jalan lengkuas no 10 RT. 009/001 Cileungsi Bogor 16360 Jawa Barat', 'andriani@gmail.com', '0219288300404');
+('215424435224', 'Bambang', 'Laki-Laki', 'bb RT. 002/001 Parung panjang bogor 16360 jawa barat', 'bambang@gmail.com', '089239219381'),
+('215424441974', 'Bejo', 'Laki-Laki', 'nfdf RT. 002/006 PARUNG PANJANG KABUPATEN ACEH SINGKIL 21312 ACEH', 'r@gmail.com', '0328230'),
+('215424558474', 'Bejo', 'Laki-Laki', 'Bogor RT. 002/001 PARUNG PANJANG KABUPATEN ACEH SINGKIL 21312 ACEH', 'r@gmail.com', '0328230'),
+('215425078974', 'Danu', 'Laki-Laki', 'Jl.Bojong kenyot RT. 002/006 PARUNG PANJANG KABUPATEN ACEH SINGKIL 21312 ACEH', 'r@gmail.com', '0328230'),
+('215425191424', 'Dono', 'Laki-Laki', 'nfdf RT. 002/006 PARUNG PANJANG KABUPATEN ACEH SINGKIL 21312 ACEH', 'r@gmail.com', '0328230');
 
 -- --------------------------------------------------------
 
@@ -65,11 +105,12 @@ CREATE TABLE `pesanan` (
 --
 
 INSERT INTO `pesanan` (`id_pesanan`, `id_pelanggan`, `id_product`, `qty`, `harga`, `tgl_pemesanan`) VALUES
-('1542385403', '215423853744', 3, 2, 105000, '2018-11-16'),
-('1542385403', '215423853744', 4, 1, 76000, '2018-11-16'),
-('1542385403', '215423853744', 1, 3, 160000, '2018-11-16'),
-('1542385991', '215423857234', 2, 2, 105000, '2018-11-16'),
-('1542385991', '215423857234', 1, 2, 160000, '2018-11-16');
+('1542443730', '215424435224', 3, 1, 105000, '2018-11-17'),
+('1542444218', '215424441974', 6, 1, 115000, '2018-11-17'),
+('1542444218', '215424441974', 2, 1, 105000, '2018-11-17'),
+('1542455867', '215424558474', 2, 1, 105000, '2018-11-17'),
+('1542507927', '215425078974', 3, 1, 105000, '2018-11-18'),
+('1542519157', '215425191424', 2, 1, 105000, '2018-11-18');
 
 -- --------------------------------------------------------
 
@@ -100,7 +141,6 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga`, `deskripsi`, `jenis_p
 (7, 'Lady Valentine', '108000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
 (8, 'Red Kochin', '130000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
 (9, 'Aglaonema Red Ruby', '108000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
-(10, 'Aglaonema Adelia', '108000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
 (11, 'Widuri', '140000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
 (12, 'aa', '12000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
 (13, 'ab', '200000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Lokal', ''),
@@ -111,6 +151,46 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga`, `deskripsi`, `jenis_p
 (18, 'aj', '90000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Thailand', ''),
 (19, 'ak', '60000', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Thailand', ''),
 (20, 'akj', '56778', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque maxime, tenetur. Molestias ratione beatae impedit eligendi quas sapiente cupiditate aut dicta doloribus expedita quisquam, dolorum exercitationem non et est laboriosam! ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis quaerat cupiditate voluptatem aliquam cum unde sapiente sint magni esse consequatur quasi, quisquam ratione. Facilis, dignissimos omnis labore eius ipsa!', 'Thailand', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rekening`
+--
+
+CREATE TABLE `rekening` (
+  `id_rekening` char(25) DEFAULT NULL,
+  `atas_nama` text,
+  `nomor_rekening` char(25) DEFAULT NULL,
+  `nama_bank` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rekening`
+--
+
+INSERT INTO `rekening` (`id_rekening`, `atas_nama`, `nomor_rekening`, `nama_bank`) VALUES
+('1542463384', 'Ranu Pirman', '2982184', 'BCA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resi`
+--
+
+CREATE TABLE `resi` (
+  `id_resi` int(25) DEFAULT NULL,
+  `id_faktur` int(25) DEFAULT NULL,
+  `nomor_resi` char(50) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `resi`
+--
+
+INSERT INTO `resi` (`id_resi`, `id_faktur`, `nomor_resi`, `tanggal`) VALUES
+(1542458995, 2147483647, 'GE76236462174', '2017-11-18');
 
 -- --------------------------------------------------------
 
@@ -159,11 +239,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `Nama`, `Email`, `Nohp`, `username`, `password`, `sandi`, `level`, `status_keadaan`, `status_data`) VALUES
-('3742387362472', 'Ranu', 'Ranu@gmail.com', '082892112717', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'Admin', '1', '1');
+('3742387362472', 'Ranu', 'Ranu@gmail.com', '082892112717', 'ranu', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'Admin', '1', '1');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `faktur`
+--
+ALTER TABLE `faktur`
+  ADD PRIMARY KEY (`id_faktur`);
 
 --
 -- Indexes for table `pelanggan`
